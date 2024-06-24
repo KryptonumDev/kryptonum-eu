d27ae6660d2522dd924667397168b5ba779f012c
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-06-24 16:42:46 +0200

    Add favicon

---
+++ astro/src/pages/favicon.ico.ts
@@ -0,0 +1,14 @@
+import type { APIRoute } from 'astro';
+import sharp from 'sharp';
+import ico from 'sharp-ico';
+
+const icon = 'src/assets/favicon.png';
+
+export const GET: APIRoute = async () => {
+  const buffer = await sharp(icon).resize(32).toFormat('png').toBuffer();
+  const icoBuffer = ico.encode([buffer]);
+
+  return new Response(icoBuffer, {
+    headers: { 'Content-Type': 'image/x-icon' }
+  })
+}
