dda4d37528db4bc0c2fe44f277340826dd6d661b
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-06-24 16:06:23 +0200

    Add robots.txt

---
+++ astro/src/pages/robots.txt.ts
@@ -0,0 +1,17 @@
+import type { APIRoute } from "astro";
+import { DOMAIN } from "@/global/constants";
+
+const content = [
+  'User-Agent: *',
+  'Allow: /',
+  '',
+  `Sitemap: ${DOMAIN}/sitemap.xml`
+].join('\n');
+
+export const GET: APIRoute = async () => {
+  return new Response(content, {
+    headers: {
+      "Content-Type": "text/plain",
+    },
+  });
+}
