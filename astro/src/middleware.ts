f265f974e19fa3a315f9429de53787e50c31ebab
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-06-27 13:27:57 +0200

    Add path.resolve to favicon.ico.ts

--- astro/src/middleware.ts
+++ astro/src/middleware.ts
@@ -1,15 +1,18 @@
-import { defineMiddleware } from "astro/middleware";
+import type { APIContext, MiddlewareHandler } from "astro";
 import { isr } from "@/utils/isr";
 
-export const onRequest = defineMiddleware(async (context, next) => {
-  const key = context.url.pathname;
+const shouldSkipCache = (req: APIContext) => {
+  if (req.request.method !== "GET") return true;
+};
+
+export const onRequest: MiddlewareHandler = async (req, next) => {
+  const key = req.url.pathname;
   console.log("[Middleware] onRequest", key);
 
   let ttl: undefined | number;
-  context.locals.cache = (seconds: number = 60) => (ttl = seconds);
+  req.locals.cache = (seconds: number = 60) => (ttl = seconds);
 
-  if (context.request.method !== "GET") return next();
-
+  if (shouldSkipCache(req)) return next();
   const cachedResponse = isr.get(key);
   if (cachedResponse) return cachedResponse;
 
@@ -17,4 +20,4 @@ export const onRequest = defineMiddleware(async (context, next) => {
   if (ttl !== undefined) isr.set(key, response, ttl);
 
   return response;
-});
+};
--- astro/src/pages/favicon.ico.ts
+++ astro/src/pages/favicon.ico.ts
@@ -1,8 +1,9 @@
 import type { APIRoute } from 'astro';
+import path from 'node:path'
 import sharp from 'sharp';
 import ico from 'sharp-ico';
 
-const icon = './src/assets/favicon.png';
+const icon = path.resolve('src/assets/favicon.png');
 
 export const GET: APIRoute = async () => {
   const buffer = await sharp(icon).resize(32).toFormat('png').toBuffer();
